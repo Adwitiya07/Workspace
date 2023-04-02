448a186dbe96f9db6891105986cc52c8698999b2
Adwitiya <adwitiyachouhan@gmail.com>
2023-04-02 11:44:19 +0530

    added the additional file

---
+++ Python/app.py
@@ -0,0 +1,4 @@
+Patient_name= "Jhon Smith"
+Patient_age= 20
+Patient_new= Truerue
+print(Patient_type,Patient_age,Patient_name)
---
+++ Python/sum.py
@@ -0,0 +1,4 @@
+First = input("Enter the first number ")
+Seconed = input("enter the second number ")
+Sum = int(First) + int(Seconed)
+print(Sum)
