946acb61859cbd043f66dea3e0591e756779fac9
Adwitiya <adwitiyachouhan@gmail.com>
2023-04-17 00:55:53 +0530

    print and string manipulation-100 days

---
+++ Python/bandname.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand Name Generator ")
+x= input("What's name of the city you grew up in? ")
+y =input("What is your pets name? ")
+print("Your band name could be ", x +" " +y)
---
+++ Python/stringconcatinating.py
@@ -0,0 +1,4 @@
+print("Hello" + "Adwi")
+print("Hello " + "Adwi")
+print("Hello" + " " + "Adwi")
+print("Hello" + " Adwi")
